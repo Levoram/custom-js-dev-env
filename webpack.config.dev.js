5487df99ee7a92ae16497bb0a308e11a72dd1f88
Nico Kuechler <mail@nico-kuechler.de>
2018-12-07 16:41:53 +0100

    Update webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,23 +1,38 @@
+import webpack from 'webpack';
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-export default{
-  debug:true,
-  devtool:'inline-source-map',
-  noInfo:false,
-  entry:[
-    path.resolve(__dirname,'src/index')
-  ],
-  target:'web',
-  output:{
-    path: path.resolve(__dirname,'src'),
-    publicPath: '/',
-    filename:'bundle.js'
+export default {
+  mode: 'development',
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json']
   },
-  plugins:[],
-  module:{
-    loaders:[
-      {test: /\.js$/, exclude:/node_modules/,loaders:['bable']},
-      {test:/\.css$/,loaders:['style','css']}
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+      noInfo: true // set to false to see a list of every file being bundled.
+    }),
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.css$/, use: ['style-loader','css-loader']}
     ]
   }
-};
+}
