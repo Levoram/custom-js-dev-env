a1652fc5f93233f4740982c621fe80bd30eb3528
Levoram <mail@nico-kuechler.de>
2018-12-08 09:01:40 +0100

    updated dependencies and review of configuration files

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,28 +1,32 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
 
-app.use(require('webpack-dev-middleware')(compiler,{
-noInfo:true,
-publicPath: config.output.publicPath
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
 }));
 
-//Serving files from filesystem
-app.get('/', function (req, res){
-  res.sendFile(path.join(__dirname,'../src/index.html'));
+//Serving static files from filesystem
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen (port, function(err){
-if(err){
-  //Log err to console
-  console.log(err);
-}else{
-  //open bowser automatically to the right port
-  open('http://localhost:'+port);
-}
+app.listen(port, function (err) {
+  if (err) {
+    //Log err to console
+    console.log(err);
+  } else {
+    //open bowser automatically to the right port
+    open('http://localhost:' + port);
+  }
 });
-
-
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,23 +1,30 @@
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-export default{
-  debug:true,
-  devtool:'inline-source-map',
-  noInfo:false,
-  entry:[
-    path.resolve(__dirname,'src/index')
+export default {
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
   ],
-  target:'web',
-  output:{
-    path: path.resolve(__dirname,'src'),
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
     publicPath: '/',
-    filename:'bundle.js'
+    filename: 'bundle.js'
   },
-  plugins:[],
-  module:{
-    loaders:[
-      {test: /\.js$/, exclude:/node_modules/,loaders:['bable']},
-      {test:/\.css$/,loaders:['style','css']}
+  plugins: [
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
     ]
   }
-};
+}
